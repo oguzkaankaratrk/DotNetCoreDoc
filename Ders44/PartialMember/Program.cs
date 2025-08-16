a9efc5c7f733cf305da42c0652ecb46439ddedbf
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2025-08-16 20:53:10 +0300

    PartialMethod

---
+++ Ders44/PartialMember/Program.cs
@@ -0,0 +1,24 @@
+﻿//partial
+//hem classlarda hemde member(method, property, indexer, eventlarda) kullanılan bir özelliktir.
+//amaç
+//bir class veya member farklı dosya veya katmanlarda yazılabilmesini sağlamak
+//geliştirici ve araçlar aynı clasa veya membera katkı sağlasın diye kullanırız
+
+//partial member aslında iki parçadan oluşmaktadır
+//declering declaration(bildiren kısım): sadece imza var gövde yok
+//implementing decleration(uygulayan kısım): imza + gövde(gerçek kod burada)
+
+//eğer implement eden kısım yok ise, compailer bildire kısmı derlemeden tamamen arabilir (void,privvate,out parametre yoksa)
+
+public partial class MyPartialClass
+{
+    //sadece bildiren kısım
+    partial void OnSamethingHappened(string s);
+}
+
+public partial class MyPartialClass
+{
+    //implement eden kısım
+    partial void OnSamethingHappened(string s) =>
+        Console.WriteLine($"Something happened: {s}");
+}
---
+++ Ders45/PartialMethod/Program.cs
@@ -0,0 +1,65 @@
+﻿//partial method un imlement edilmesi zorunlu değildir. yalnızca koşullar sağlıyorsa
+//void döner
+//genelde private kullanılır
+//out parametresi yok
+//virtual, override, sealded, new, extern kullanılmaz
+
+
+
+//kurallara uyan, implemente edilmesi zorunlu olmayan partial method
+public partial class Person
+{
+    //sadece imza, private void, erişim belirteci yok.
+    partial void OnNameCahnged(string newName);
+
+    private string _name;
+
+    private string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+
+            OnNameCahnged(value);
+        }
+    }
+}
+
+//imlemente etmek zorunlu değildir.
+public partial class Person
+{
+    //implemente et
+    partial void OnNameCahnged(string newName)
+    {
+        Console.WriteLine($"name chaged to: {newName}");
+    }
+}
+
+//eğer ikinci kısımdaki implementasyonu silersen
+//OnNameCahnged ve tüm çağrıları derlemede tamamen yok edilir.
+//kod yine çalışır hiçbir hata vermez.
+
+
+//bunlardan biri ihlal edilirse(ör public partial string GetName()), ozaman implemente edilmek zorundadır.
+
+
+//Kuralları ihlal eden, implemente edilmesi zorunlu olan partial method.
+public partial class Employee
+{
+    //kuralları ihlal ediyor
+    //__public var, string dönüyor
+    public partial string GetName();
+}
+
+public partial class Employee
+{
+    private string _name = "mushab";
+
+    //bu imlemantasyon yazılmak zorunludur.
+    //aksi halde derleyici hata verir. 
+    public partial string GetName()
+    {
+        return _name;
+    }
+}
