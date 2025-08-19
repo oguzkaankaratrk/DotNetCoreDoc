eb2e88162ca2ee92a86bedc9a56fd3f03a18fca7
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2025-08-19 23:21:14 +0300

    GenericUseInNestedType

---
+++ Ders49/GenericUseInNestedType/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Box<T>
+{
+
+    class Holder
+    {
+        public T? Value;//Dışarıdaki t yi içeride kullanabiliyoruz.
+    }
+}
+
+//Nested type i static olaraktanımlayabişliriz
+public static class NestedTypes
+{
+
+}
+
+//Nerede kullanılır
+//Uygulama detayını saklamak, 
+//Enumerator/Iterator : IEnumurable<T> Uygularken, özel enumarator sınıfını nested private olarak tanımlarız. 
+//Builder/ Comparer / Policy: Dış sınıfa özgü strateji ve builder nesnelerini içeride saklamak .
+//Yardımcı bir tipinva, name space şişmesini önlemek istiyorsan nested tip kullanırsın. 
+//
