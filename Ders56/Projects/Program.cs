9bca30e0c87d63394e743aaa40ebcffa316d0556
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2025-09-07 19:27:42 +0800

    Ders56

---
+++ Ders56/Projects/Program.cs
@@ -0,0 +1,39 @@
+﻿//ConsoleApp : Console projelerini geliştirmek için kullanılır. (terminalden çalıştırırız, poweshell, cmd)
+//Test için, ders anlatmak, basit yazılımlar geliştirmek için kullanılır 
+
+//Blazor app : tarayıcıda çalışan kodlar için kullanılır. 
+//react, anguların c# versiyonudur. 
+
+//Aspire starter app : daha küçük projeler için hızlı başlangıç
+//blazor frontend, api backend test için kullanılabilir.
+//orta ölçekli projeler için bir starter kittir. 
+
+//Asp.net core web app (razor page)
+//razor page tabanlı uygulama geliştirmek için kullanılır
+//mvc ye benzer ama daha basit sayfalar için kullanılır
+//orta ölçekli projeler için kullanılır
+
+//web api : rest ful api geliştirmek içik için kullanılır. backend micro servicelerde kullanılır ....
+
+//Web api native aot : yine web api, normalden farı
+//kodu daha önceden derlenmiş native executable olarak çıkartır. 
+//daha hızlıdır. container tabanlı, preformans  önemli olan, serverless  dağıtımlar için kullanılır
+
+//class library : kodunu kütüphane yapıcan kullanıcan. 
+//blazora, web apiye, konsola, referans ekler yazdığımız kodları burada kullanabiliriz. 
+
+//azure fuctions : serverless uygulamalar geliştirmek için kullanılır
+//azure claudunda ölçeklenebilir. 
+//mikro görevler için idealdır.hafif servisler.
+//webhook işleme, arka plan cron jobları mikro servis fonksiyonlarında kullanılır
+
+//grpc : grpc protokolü ile yüksek performanslı servisler geliştirmek için kullanılır. 
+//rest yerine google remote procedure call kullanılır. 
+//json yerine protocol buffer kullanacağız. 
+
+//razor class library : razor bileşenlerini yeniden kullanılabilir yapmak için kullanılır. 
+//blazor, razor page projelerinde kullanılır
+
+//worker services : arka planda çalışan uzun süreli servisler için kullanılır
+//log ileme, mesaj kuyruk sistemi, kuyruk dinlemeemail sender, veri işleme servislerinde kullanılır
+
