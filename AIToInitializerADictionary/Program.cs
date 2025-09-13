3351c4184f573a5d015895d49c6c442f93a9582f
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2025-09-13 20:15:51 +0800

    Dictionaryİnitialize

---
+++ AIToInitializerADictionary/Program.cs
@@ -0,0 +1,9 @@
+﻿//Kodlarımızı yapay zeka ile yazarken bazı kolaylıklar vardır. 
+//bunu nasış sağlayacağız
+
+
+///Generate C# code to initialize Dictionary<int, Employee> using key-value pairs
+///within the collection initializer. The employee class is a record class with two
+///properties: Name and Age.
+
+///
---
+++ Ders57/DictionaryInitializer/Program.cs
@@ -0,0 +1,28 @@
+﻿using static DictionaryInitializer.ToDictionaryInitializer;
+
+static void Main()
+{
+    var students = new Dictionary<int, StudentName>()
+    {
+        {111, new StudentName{Name="mushab",LastName="Güldemir",ID=1123} },
+        {112, new StudentName{Name="murat",LastName="demir",ID=123}}
+    };
+
+    foreach (var index in Enumerable.Range(112, 3))
+    {
+        Console.WriteLine($"student {index} is {students[index].Name} {students[index].LastName}");
+    }
+    Console.WriteLine();
+
+    var students2 = new Dictionary<int, StudentName>()
+    {
+        [111] = new StudentName{Name="mushab",LastName="Güldemir",ID=1123},
+        [112] = new StudentName{Name="murat",LastName="demir",ID=123}
+    };
+
+    foreach (var index in Enumerable.Range(1123, 3))
+    {
+        Console.WriteLine($"student {index} is {students2[index].Name} {students2[index].LastName}");
+    }
+    Console.WriteLine();
+}
